d5020c371c4042f92fdfbb0b79350c044eeafd59
shinyfromage <seobotxr11@gmail.com>
2023-05-01 09:57:48 -0400

    setting up basic nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '/home', component: MovieComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
